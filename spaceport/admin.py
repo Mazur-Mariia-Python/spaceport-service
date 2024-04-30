b4099b94112d93ca84813d9083ccd66e942ccb5f
ProjectTobi <mariiamazur2927@gmail.com>
2024-04-30 20:37:48 +0300

    Updated admin.py for spaceport app

--- spaceport/admin.py
+++ spaceport/admin.py
@@ -1,3 +1,33 @@
 from django.contrib import admin
 
-# Register your models here.
+from spaceport.models import (
+    Ticket,
+    Order,
+    SpaceshipType,
+    Crew,
+    Spaceship,
+    Planet,
+    Spaceport,
+    Route,
+    Spaceflight,
+)
+
+
+class TicketInLine(admin.TabularInline):
+    model = Ticket
+    extra = 1
+
+
+@admin.register(Order)
+class OrderAdmin(admin.ModelAdmin):
+    inlines = (TicketInLine,)
+
+
+admin.site.register(SpaceshipType)
+admin.site.register(Crew)
+admin.site.register(Spaceship)
+admin.site.register(Planet)
+admin.site.register(Spaceport)
+admin.site.register(Route)
+admin.site.register(Spaceflight)
+admin.site.register(Ticket)
